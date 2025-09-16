ed7e87b7c53b031400d095faf359663e8a340bea
syseo0107 <syseo0107@gmail.com>
2025-09-16 22:03:48 +0900

    ci: chromatic deploy

--- storybook/.storybook/preview.js
+++ storybook/.storybook/preview.js
@@ -1,45 +1,4 @@
-/** @type { import('@storybook/react-vite').Preview } */
 // .storybook/preview.js
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import { withThemeFromJSXProvider } from '@storybook/addon-themes';
-import { lightTheme, darkTheme } from '../src/themes.js';
-
-/* snipped for brevity */
-
-export const decorators = [
-  withThemeFromJSXProvider({
-    themes: {
-      light: lightTheme,
-      dark: darkTheme,
-    },
-    defaultTheme: 'light',
-    Provider: ThemeProvider,
-    GlobalStyles: CssBaseline,
-  }),
-];
-
-const preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-    a11y: {
-      // 'todo' - show a11y violations in the test UI only
-      // 'error' - fail CI on a11y violations
-      // 'off' - skip a11y checks entirely
-      test: 'todo'
-    },
-    // 추가 권장 설정
-    backgrounds: {
-      disable: true, // 테마 전환을 사용하므로 배경색 컨트롤 비활성화
-    },
-    docs: {
-      theme: lightTheme, // 문서 페이지에서 일관된 테마 사용
-    },
-  },
+export const parameters = {
+  controls: { expanded: true },
 };
-
-export default preview;
---
+++ storybook/src/Button.stories.js
@@ -0,0 +1,10 @@
+import Button from './Button';
+
+export default {
+  title: 'Example/Button',
+  component: Button,
+};
+
+export const Primary = {
+  args: { label: 'Hello Storybook' },
+};
